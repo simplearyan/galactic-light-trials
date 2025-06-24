020db8948ebb9ff31c7bcd8119b0b759912a0285
simplearyan <aryanphone00620@gmail.com>
2025-06-24 14:15:12 +0530

    intial Commit

---
+++ js/canvas.js
@@ -0,0 +1,111 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+console.log(innerHeight);
+
+const mouse = {
+  x: innerWidth / 2,
+  y: innerHeight / 2,
+};
+
+const colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
+
+// Event Listeners
+let mouseDown = false;
+addEventListener("mousedown", () => {
+  mouseDown = true;
+});
+addEventListener("mouseup", () => {
+  mouseDown = false;
+});
+addEventListener("touchstart", (e) => {
+  mouseDown = true;
+});
+addEventListener("touchend", () => {
+  mouseDown = false;
+});
+
+addEventListener("resize", () => {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+
+  init();
+});
+
+// Objects
+class Particle {
+  constructor(x, y, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+  }
+
+  draw() {
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.shadowColor = this.color;
+    c.shadowBlur = 15;
+    c.fillStyle = this.color;
+    c.fill();
+    c.closePath();
+  }
+
+  update() {
+    this.draw();
+  }
+}
+
+// Implementation
+let particles;
+function init() {
+  particles = [];
+
+  for (let i = 0; i < 600; i++) {
+    const canvasWidth = canvas.width + 300; // 900 for mobile
+    if (canvas.width < 600) {
+      canvasWidth = canvas.width + 900; // 300 for mobile
+    }
+    const canvasHeight = canvas.height + 900; // 900 for desktop
+    // Generate random x, y, radius, and color for each particle
+    const x = Math.random() * canvasWidth - canvasWidth / 2;
+    const y = Math.random() * canvasHeight - canvasHeight / 2;
+    const radius = Math.random() * 2;
+    const color = colors[Math.floor(Math.random() * colors.length)];
+    // Create new particle and push it to the particles array
+    particles.push(new Particle(x, y, radius, color));
+  }
+}
+
+// Animation Loop
+let radians = 0;
+let alpha = 1;
+function animate() {
+  requestAnimationFrame(animate);
+  c.fillStyle = `rgba(0, 2, 7, ${alpha})`; // Adjust alpha for fade effect
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  c.save();
+  c.translate(canvas.width / 2, canvas.height / 2);
+  c.rotate(radians);
+  particles.forEach((particle) => {
+    particle.update();
+  });
+  c.restore();
+
+  // Update radians and alpha
+  radians += 0.008;
+
+  if (mouseDown && alpha >= 0.07) {
+    alpha -= 0.01; // Decrease alpha when mouse is down
+  }
+  else if (!mouseDown && alpha < 1) {
+    alpha += 0.01; // Increase alpha when mouse is not down
+  }
+}
+
+init();
+animate();
