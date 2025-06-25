7e64780d10f42a9583811ccbe11657aeb80431c1
simplearyan <aryanphone00620@gmail.com>
2025-06-25 17:25:30 +0530

    Made responsive for mobile devices | Add README.md & LICENSE

---
+++ README.md
@@ -0,0 +1,33 @@
+# Galactic Light Trails 🌌
+
+![Galactic Light Trails](https://raw.githubusercontent.com/simplearyan/stick-hero/main/assets/Screenshot.png)
+
+## Description 📒
+
+Galactic Light Trails is a mesmerizing web-based animation that simulates the movement of light trails in a galactic space environment. The animation features a dynamic background with stars and glowing trails that create an immersive visual experience.
+
+## Technologies Used 🛠️ 👨‍💻
+
+- HTML5
+- CSS3
+- JavaScript (ES6+)
+- Canvas API
+
+### Credits 🙏
+
+- Developed by [Chris Courses](https://www.youtube.com/c/chriscourses)
+- Originally created by [Chris Courses](https://codepen.io/chriscourses/pen/aZqrGr) on CodePen
+
+#### Extra Credits
+
+- [Aryan](https://github.com/simplearyan) for additional improvements and testing
+
+### License 📜
+
+This project is licensed under the MIT License - see the [LICENSE]([LICENSE](https://github.com/simplearyan/stick-hero/blob/main/LICENSE)) file for details.
+
+### Contact 📫
+
+For any questions or feedback, feel free raise an issue on the [GitHub repository](https://github.com/simplearyan/stick-hero)
+
+
--- js/canvas.js
+++ js/canvas.js
@@ -1,14 +1,20 @@
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
 
-console.log(innerHeight);
+console.log('Width:', canvas.width, 'Height:', canvas.height);
+
+var particleCount = 750; // Default particle count
+
+if (canvas.width < 980) {
+  particleCount = 900; // Increase particle count for mobile
+}
 
 const mouse = {
-  x: innerWidth / 2,
-  y: innerHeight / 2,
+  x: window.innerWidth / 2,
+  y: window.innerHeight / 2,
 };
 
 const colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
@@ -29,8 +35,8 @@ addEventListener("touchend", () => {
 });
 
 addEventListener("resize", () => {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
   init();
 });
@@ -64,26 +70,39 @@ let particles;
 function init() {
   particles = [];
 
-  for (let i = 0; i < 600; i++) {
-    const canvasWidth = canvas.width + 300; // 900 for mobile
-    if (canvas.width < 600) {
+  for (let i = 0; i < particleCount; i++) {
+    var canvasWidth = canvas.width + 200; // 900 for mobile
+
+    if (canvas.width < 990) {
       canvasWidth = canvas.width + 900; // 300 for mobile
     }
-    const canvasHeight = canvas.height + 900; // 900 for desktop
+
+    const canvasHeight = canvas.height + 200; // 900 for desktop
+
+    if (canvas.width < 990) {
+      canvasHeight = canvas.height + 900; // 300 for mobile
+    }
+
     // Generate random x, y, radius, and color for each particle
     const x = Math.random() * canvasWidth - canvasWidth / 2;
     const y = Math.random() * canvasHeight - canvasHeight / 2;
-    const radius = Math.random() * 2;
+
+    const radius = Math.random() * 2 ; // Random radius between 1 and 3
+
     const color = colors[Math.floor(Math.random() * colors.length)];
+
     // Create new particle and push it to the particles array
     particles.push(new Particle(x, y, radius, color));
+
   }
 }
 
 // Animation Loop
 let radians = 0;
 let alpha = 1;
+
 function animate() {
+
   requestAnimationFrame(animate);
   c.fillStyle = `rgba(0, 2, 7, ${alpha})`; // Adjust alpha for fade effect
   c.fillRect(0, 0, canvas.width, canvas.height);
@@ -99,11 +118,10 @@ function animate() {
   // Update radians and alpha
   radians += 0.008;
 
-  if (mouseDown && alpha >= 0.07) {
+  if (mouseDown && alpha >= 0.05) {
     alpha -= 0.01; // Decrease alpha when mouse is down
-  }
-  else if (!mouseDown && alpha < 1) {
-    alpha += 0.01; // Increase alpha when mouse is not down
+  } else if (!mouseDown && alpha < 1 && alpha >= 0.5) {
+    alpha += 0.001; // Increase alpha when mouse is not down
   }
 }
 
